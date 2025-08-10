837f7b4ba6cdfc279937d637ac67f31b6c93d433
ankitkrshah30 <ankitkrshah30@gmail.com>
2025-08-10 23:58:28 +0530

    fixedWebConfig

--- aas-paas/backend/src/main/java/com/aaspaas/backend/config/WebConfig.java
+++ aas-paas/backend/src/main/java/com/aaspaas/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Allow CORS for all endpoints under /api
-                .allowedOrigins("http://localhost:3000") // Allow requests from our React app
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(false);
